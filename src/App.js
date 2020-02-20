e31fe4b7534f2cd730ec2aef37fc15ea6acbc3bb
banobepascal <banobepascal@gmail.com>
2020-02-20 14:26:08 +0300

    ft(rendering todos):
    - created todo component
    - added todo state
    
    Finishes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import Todos from "./components/Todos";
+
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Going to a meeting",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Picking up some groceries",
+        completed: true
+      },
+      {
+        id: 3,
+        title: "Call my boss",
+        completed: false
+      }
+    ]
+  };
   render() {
     return (
       <div className="App">
         <h1>Hello React</h1>
+        <Todos todos={this.state.todos}></Todos>
       </div>
     );
   }
