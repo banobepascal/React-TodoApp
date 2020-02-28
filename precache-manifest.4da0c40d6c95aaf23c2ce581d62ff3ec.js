5c556abb31ec591dfaa24a20325ee235d4a06aa5
banobepascal <banobepascal@gmail.com>
2020-02-29 00:26:02 +0300

    Updates

---
+++ precache-manifest.4da0c40d6c95aaf23c2ce581d62ff3ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f87c022ded69daa2d883e5e49df714",
+    "url": "/React-TodoApp/index.html"
+  },
+  {
+    "revision": "15999b53547a6d4bd1f7",
+    "url": "/React-TodoApp/static/css/main.1c0c892d.chunk.css"
+  },
+  {
+    "revision": "b2ed7a2f2f310da9766b",
+    "url": "/React-TodoApp/static/js/2.c104b0a3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-TodoApp/static/js/2.c104b0a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15999b53547a6d4bd1f7",
+    "url": "/React-TodoApp/static/js/main.03fcaf34.chunk.js"
+  },
+  {
+    "revision": "ed9ad6b891cd6f260950",
+    "url": "/React-TodoApp/static/js/runtime-main.4d0f84f8.js"
+  }
+]);
